2449354f145f1e43470744ef238dea44674f3a83
hoanthien <hoanthien97@gmail.com>
2019-09-23 21:47:05 +0700

    2 tabs with film schedule and information

--- app/src/main/java/com/example/tickee/FilmScheduleActivity.java
+++ app/src/main/java/com/example/tickee/FilmScheduleActivity.java
@@ -1,22 +1,61 @@
 package com.example.tickee;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
 import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class FilmScheduleActivity extends AppCompatActivity {
-
+    VideoView vid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_film_schedule);
+        TabLayout tabLayout = findViewById(R.id.tabTitle);
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.detailsContainer, new FilmScheduleFragment());
+        transaction.commit();
+        tabLayout.addOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Fragment fragment = null;
+                switch (tab.getText().toString()){
+                    case "Lịch chiếu":
+                        fragment = new FilmScheduleFragment();
+                        break;
+                    case "Thông tin":
+                        fragment = new FilmInformationFragment();
+                        break;
+                }
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.detailsContainer, fragment);
+                transaction.commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 //        CinemaShortDescriptionFragment fragment = new CinemaShortDescriptionFragment("BHD","Hai Bà Trưng", "0.5 km");
 //        CinemaShortDescriptionFragment fragment2 = new CinemaShortDescriptionFragment("Lotte","Trần Hưng Đạo", "1 km");
 //        CinemaShortDescriptionFragment fragment3 = new CinemaShortDescriptionFragment("CNS","Lê Lợi", "2 km");
@@ -29,9 +68,8 @@ public class FilmScheduleActivity extends AppCompatActivity {
 //        transaction.add(R.id.cinemaContainer,fragment5);
 //        transaction.commit();
 
-    }
+
     public void onClickShowMore(View view){
-        Toast.makeText(this, "view " + view.getId(), Toast.LENGTH_SHORT).show();
         LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         ViewGroup parent = findViewById(view.getId());
         parent.setMinimumHeight(70);
