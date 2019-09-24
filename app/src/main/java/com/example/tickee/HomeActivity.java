a856056db85d723c00a6d0c331dbb6f26166c3ac
cuonghxse63139 <cuonghxse63139@fpt.edu.vn>
2019-09-24 16:56:25 +0700

    Mat me thanh navigatiob roi ai sua dum voi

--- app/src/main/java/com/example/tickee/HomeActivity.java
+++ app/src/main/java/com/example/tickee/HomeActivity.java
@@ -2,19 +2,23 @@ package com.example.tickee;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
 
-public class HomeActivity extends AppCompatActivity {
-
+public class    HomeActivity extends AppCompatActivity {
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initView();
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigationBar);
         bottomNavigationView.setSelectedItemId(R.id.nav_home);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -40,6 +44,13 @@ public class HomeActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    private void initView() {
+        viewPager = findViewById(R.id.homeVP);
+        viewPager.setAdapter(new HomeAdapter(getSupportFragmentManager()));
+        TabLayout tabLayout = findViewById(R.id.homeTab);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
 }
