4176b28e1540f839832fb502dc7f22722e490b36
cuonghxse63139 <cuonghxse63139@fpt.edu.vn>
2019-09-29 19:03:08 +0700

    Man hinh hoi xau, ai kiem them hinh voi

--- app/src/main/java/com/example/tickee/NCFragment.java
+++ app/src/main/java/com/example/tickee/NCFragment.java
@@ -8,11 +8,17 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class NCFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.ngungchieu, container, false);
+        View view = inflater.inflate(R.layout.ngungchieu, container, false);
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.filmNCContainer,new FilmNCFragment(R.drawable.faf));
+        transaction.add(R.id.filmNCContainer,new FilmNCFragment(R.drawable.download));
+        transaction.commit();
+        return view;
     }
 }
--- app/src/main/java/com/example/tickee/SCFragment.java
+++ app/src/main/java/com/example/tickee/SCFragment.java
@@ -8,11 +8,17 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class SCFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.sapchieu, container, false);
+        View view = inflater.inflate(R.layout.sapchieu, container, false);
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.filmSCContainer,new FilmSCFragment(R.drawable.faf));
+        transaction.add(R.id.filmSCContainer,new FilmSCFragment(R.drawable.download));
+        transaction.commit();
+        return view;
     }
 }
